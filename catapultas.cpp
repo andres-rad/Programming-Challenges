e50bd3fc5ec23ed784057253790f53b932f935f1
andres-rad <aradunsky@dc.uba.ar>
2016-05-28 01:44:00 -0300

    ble

--- catapultas.cpp
+++ catapultas.cpp
@@ -6,15 +6,15 @@
 #define y second
 using namespace std;
 
-int main{
+int main(){
 
   long n, px, py, lx, ly;
 
   while(cin >> n >> px >> py >> lx >> ly){
 
-    vector<pair<long, long> > cat;
-    vector<long> fs;
-    vector<long> dist;
+    vector<pair<int, int> > cat;
+    vector<int> fs;
+    vector<double> dist;
     vector<bool> visitado;
 
     if(n == -1) return 0;
@@ -23,19 +23,19 @@ int main{
     fs.push_back(0);
     dist.push_back(0);
     visitado.push_back(true)
-    long cx, cy, f;
+    int cx, cy, f;
 
     for(int i = 0; i < n; i++){
       cin >> cx >> cy >> f;
       cat.push_back(make_pair(cx, cy));
       fs.push_back(f);
-      dist.push_back(9999999999999999999);
+      dist.push_back(9999999999);
       visitado.push_back(false);
     }
 
     cat.push_back(make_pair(lx, ly));
     fs.push_back(0);
-    dist.push_back(9999999999999999999);
+    dist.push_back(9999999999);
     visitado.push_back(false);
 
     for(int i = 0; i < n+2; i++){
@@ -43,14 +43,14 @@ int main{
       visitado[i] = true;
       for(int j = 0; j < n+2; j++){
         if(!visitado[j]){
-          float d = abs( pow (pow(cat[curr].x - cat[j].x,2) + pow(cat[curr].y - cat[j].i, 2), 0.5) - fs[curr]);
+          double d = abs( pow (pow(cat[curr].x - cat[j].x,2) + pow(cat[curr].y - cat[j].y, 2), 0.5) - fs[curr]);
           if (dist[j] > dist[curr] + d){
             dist[j] = dist[curr] + d;
           }
         }
       }
       for(int j = 0; j < n; j++){
-        double minCurr = 999999999999999999999;
+        double minCurr = 9999999999;
         if(!visitado[j] && dist[j] < minCurr){
           curr = j;
         }
