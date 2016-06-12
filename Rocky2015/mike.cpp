54a5e3cf462bc0110ac69f632f79bde22f29da9b
andres-rad <aradunsky@dc.uba.ar>
2016-06-11 23:07:20 -0300

    Tira Wrong Answer

--- Rocky2015/mike.cpp
+++ Rocky2015/mike.cpp
@@ -7,51 +7,82 @@
 #include <queue>
 #include <map>
 #include <set>
+#include <utility>
+#include <functional>
 
 using namespace std;
 
 #define forn(i,n) for (int i=0;i<n;i++)
 #define pb push_back
-#define matriz vector<vector<char> >
 #define mp make_pair
 int main () {
 
 	int t=0, tests;
 	cin>>tests;
 
-
-
 	while (t<tests){
 		t++;
+		priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > mano;
+		pair<int, int> carta;
 		int p,q;
-		int votos=0;
-		
 		int n;
 		cin>>n;
-		vector<pair <int, int> > mano;
-		forn (i,n){
-			pair<int, int> carta;
-			cin>>p>>q;
-			carta = mp(p, q);
-			mano.pb(carta);
+		if(n==0){
+			cout << "possible" << endl;
 		}
-		sort();
+		else{
 
+			forn (i,n){
+				cin>>p>>q;
+				carta = mp(min(p,q), max(p,q));
+				mano.push(carta);
+			}
+			vector<set<int> > conexos;
+			vector<int> cuenta;
 
+			carta = mano.top();
+			mano.pop();
 
-		if (empate){
-			cout<<"no winner"<<endl;
-		}else if (2*max>votos){
-			cout<<"majority winner "<<maxi<<endl;
+			set<int> primero;
 
-		}else {
-			cout<<"minority winner "<<maxi<<endl;
-		}
+			primero.insert(carta.first);
+			primero.insert(carta.second);
+
+			conexos.push_back(primero);
+			cuenta.push_back(1);
+
+			while(!mano.empty()){
+				carta = mano.top();
+				mano.pop();
+				unsigned int j = 0;
+				while(j < conexos.size()){
+					if(carta.first == *conexos[j].find(carta.first) || carta.second == *conexos[j].find(carta.second)){
+						conexos[j].insert(carta.first);
+						conexos[j].insert(carta.second);
+						cuenta[j]++;
+						break;
+					}
+					j++;
+					if(j == conexos.size()-1){
+						set<int> nuevo;
+						nuevo.insert(carta.first);
+						nuevo.insert(carta.second);
+						conexos.push_back(nuevo);
+						cuenta.push_back(1);
+						break;
+					}
+				}
+			}
+			bool todos_bien = true;
+			forn(i, cuenta.size()){
+				if(cuenta[i] > conexos[i].size()){
+					todos_bien = false;
+					break;
+				}
+			}
+		todos_bien ? (cout << "possible") : (cout << "impossible");
+		cout << endl;
 	}
-
-		
-
-
-
+}
 	return 0;
-	}
+}
