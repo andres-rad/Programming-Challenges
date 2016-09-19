d1061acf19cf21e2333029126f3226aff907063b
Teodoro Freund <tfreund95@gmail.com>
2016-09-19 08:59:11 -0300

    bajo

--- Practica/cf651a.cpp
+++ Practica/cf651a.cpp
@@ -1,5 +1,14 @@
 //touch {a..m}.in; tee {a..m}.cpp < template.cpp
-#include <bits/stdc++.h>
+//#include <bits/stdc++.h>
+
+#include <iostream>
+#include <stdio.h>
+#include <iomanip>
+#include <math.h>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <map>
 
 using namespace std;
 
@@ -20,33 +29,34 @@ const int MAXN=100100;
 int n;
 
 int main() {
-	int n;
-    while(cin >> n){
-    
+	int n,m;
+    while(cin >>m>> n){
+
 		vector<vector<int> > res (104, vector<int> (104,0));
-		
-		forr (i,1,101){
-			forr(j,1,101){
-				
-				res[i][j]= max (res[i+1][max(j-2,0)], res[max(i-2,0)][j+1]) +1;
+
+		forr (i,1,201){
+			forr(j,1,i){
+				cout<<i-j<<' '<<j<<endl;
+
+				res[min(max(0,i-j),100)][min(max(0,j),100)]= max(res[min(100,i-j+1)][max(min(100,j-2),0)], res[max(min(100,i-j-2),0)][min(100,j+1)]) +1;
 			}
 		}
-		
+
 		forn (i,101){
 			forn (j,101){
 				cout<<res[i][j]<<' ';
 			}
 			cout<<endl;
 		}
-		
+
 		cout<<endl<<endl;
-		
-		cout<<res[3][5]<<endl<<res[4][4];
-			
-		
-		
-		
+
+		cout<<res[n][m]<<endl;
+
+
+
+
     }
-    
+
     return 0;
 }
