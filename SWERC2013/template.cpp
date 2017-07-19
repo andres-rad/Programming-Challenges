598ed60d47934fce9a05e6a06a6fb124ec84affe
Teo Freund <tfreund95@gmail.com>
2017-07-19 13:26:30 -0300

    swerc2014

---
+++ SWERC2013/template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define dprint(v) cout << #v"=" << v << endl //;)
+#define forr(i,a,b) for(int i=(a); i<(b); i++)
+#define forn(i,n) forr(i,0,n)
+#define dforn(i,n) for(int i=n-1; i>=0; i--)
+#define forall(it,v) for(auto it=v.begin();it!=v.end();++it)
+#define sz(c) ((int)c.size())
+#define zero(v) memset(v, 0, sizeof(v))
+#define pb push_back
+#define fst first
+#define snd second
+#define INF 1e9
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef double tipo;
+/////
+
+int main() {
+
+  return 0;
+}
