310b6b21ff840d37ad45bfc8d4552574300ebd15
Teo Freund <tfreund95@gmail.com>
2017-10-26 17:46:43 -0300

    agrego ej h

--- RegLATAM2010/h.cpp
+++ RegLATAM2010/h.cpp
@@ -13,12 +13,74 @@ typedef long long tint;
 #ifdef DEBUG
 #define debug(v) cerr << #v << ": " << v << endl;
 #else
-#define debug(v) 
+#define debug(v)
 #endif
 
+
 int main() {
-	ios::sync_with_stdio(0); cin.tie(0);
+    ios::sync_with_stdio(0); cin.tie(0);
 
+    int n, m;
+    cin >> m>> n;
 
-	return 0;
+    while (n!= 0){
+        map<int, pair<vector<int>, vector<int> > > comp;
+        vector<pair<int, int> > sf;
+        comp[0] = {vector<int>(), vector<int>()};
+        comp[m] = {vector<int>(), vector<int>()};
+
+        forn(i,n){
+            int s, f;
+            cin >> s>> f;
+            comp[s].fst.pb(sf.size());
+            comp[f].snd.pb(sf.size());
+            sf.pb({s,f});
+
+        }
+        int order = 0;
+        for (auto it : comp){
+            //int order = it.fst;
+            for (auto s : it.snd.fst){
+                sf[s].fst = order;
+                debug(sf[s].fst);
+            }
+            for (auto f : it.snd.snd){
+                sf[f].snd = order;
+                debug(sf[f].snd);
+            }
+            order++;
+        }
+
+        vector<vector<int> > dp (order, vector<int> (order, 0));
+        dp[0][0] = 1;
+        sort(sf.begin(), sf.end());
+
+        for (auto r:sf){
+            debug(r.fst);
+            debug(r.snd);
+            if(r.fst == 0){
+                dp[0][r.snd] ++;
+            }else{
+                forn(i, r.fst){
+                    forsn(j, r.fst, r.snd){
+                        dp[j][r.snd] += dp[i][j];
+                        dp[j][r.snd] %= 100000000;
+
+                    }
+                }
+            }
+            debug(dp[r.fst][r.snd]);
+        }
+
+        int res = 0;
+        order--;
+        forn(i, order){
+            res += dp[i][order];
+            res %= 100000000;
+        }
+        cout << res << endl;;
+        cin >>m >> n;
+    }
+
+    return 0;
 }
