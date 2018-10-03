5d163e779fa41ad10f757d138973577756f91d62
Teo Freund <tfreund95@gmail.com>
2018-10-03 17:35:09 -0300

    CERC 17

---
+++ CERC17/template.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define forsn(i,s,n) for(long i=s; i<n; i++)
+#define forn(i,n) forsn(i,0,n)
+#define debug(v) cerr << #v << ": " << (v) << endl
+#define fst first
+#define snd second
+typedef long long tint;
+
+
+int main() {
+	int n;
+
+	while(cin >> n){
+		
+	}	
+	return 0;
+}
