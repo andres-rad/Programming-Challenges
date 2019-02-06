75f3da857c1892db47f7851677bb4e6ee531ec6b
eugenusb <eugenusb@gmail.com>
2019-02-06 09:41:09 -0300

    Subo el B con la observacion de Teo

--- Codejam2018/b.cpp
+++ Codejam2018/b.cpp
@@ -1,159 +1,210 @@
 #include <bits/stdc++.h>
-using namespace std;
-typedef long long tint;
-#define forsn(i, s, n) for (tint i = (tint)(s); i < (tint)(n); i++)
-#define forn(i, n) forsn(i, 0, n)
-#define fst first
-#define snd second
+
+#define forn(i,n) for(int i = 0; i < (n); i++)
+#define forsn(i,s,n) for(int i = (s); i < (n); i++)
+#define sq(x) ((x) * (x))
+#define all(v) ((v).begin, (v).end)
 #define pb push_back
+#define f first
+#define s second
+#define mp make_pair
+#define EPS 1e-10
 
-#ifdef DEBUG
-#define debug(v) cerr << #v << ": " << (v) << endl;
-#else
-#define debug(v)
-#endif
+using namespace std;
+typedef pair<int,int> par;
+typedef long long int tint;
 
-const tint maxN = 501;
+struct pair_hash
+{
+	std::size_t operator () (par const &p) const
+	{
+		std::size_t h1 = std::hash<int>()(p.first);
+		std::size_t h2 = std::hash<int>()(p.second);
 
-struct data {
-    tint res;
-    tint rSob;
-    tint bSob;
-    tint rNew;
-    tint bNew;
-    bool fatherB;
+		return (h1 * 35 + h2);
+	}
 };
 
-vector<vector<data>> dp;
+int dp[510][510]; 
+// dp[i][j] = maxima cantidad de jugglers si hay i cadenas azules y j rojas. Llamo a esto estado (i,j)
+par p[510][510];
+// p[i][j] = "padre" del estado (i,j). Apunta al estado (k,l) anterior a (i,j) en una cadena optima. Eso implica haber agregado el par (i-k,j-l)
+//            para pasar de (k,l) a (i,j) o simplemente ignorar el excedente y usar solo las cadenas del estado (k,l). Depende de a.
+bool a[510][510];
+// a[i][j] = true si y solo si para pasar del estado (k,l) = p[i][j] a (i,j) hay que agregar el par (i-k,j-l)
 
-bool isInSet(tint r, tint b, tint currR, tint currB) {
-    if (currR == 0 and currB == 0)
-        return false;
-    if (r == 0 and b == 0)
-        return false;
+//unordered_set< par, pair_hash > used[510][510];
 
-    const data d = dp[currR][currB];
-    if (d.rNew == r and d.bNew == b)
-        return true;
+int main()
+{
+	int t,r,b;
 
-    tint nextR = d.fatherB ? currR : currR - 1;
-    tint nextB = d.fatherB ? currB - 1 : currB;
-    return isInSet(r, b, nextR, nextB);
-}
+	cin >> t;
 
-void mkDP(tint r, tint b) {
-    if (r == 0 and b == 0)
-        return;
+	int B = 501; int R = 501;
+	
+	// inicializo dp en la fila 0 y la columna 0. Si hay una sola dimension las unicas transiciones interesantes van a ocurrir en los numeros
+	// triangulares, es decir, en los numeros x de la pinta x = n * (n + 1) / 2 para algun n.
 
-    const data fR = r > 0 ? dp[r - 1][b] : data{};
-    const data fB = b > 0 ? dp[r][b - 1] : data{};
+	dp[0][0];
+	p[0][0] = mp(-1,-1);
+	a[0][0] = false;
+	forsn(i,1,B)
+	{
+		int prev = dp[i-1][0];
+		dp[i][0] = prev;
+		a[i][0] = false;
+		p[i][0] = p[i-1][0];
+		//unordered_set< par, pair_hash >:: iterator it;
+		//for(it = used[i-1][0].begin(); it != used[i-1][0].end(); it++)
+		//	used[i][0].insert(*it);
+		int tot = ( (prev) * (prev + 1) ) / 2;
+		if(i - tot >= (prev + 1))
+		{
+			dp[i][0]++;
+			a[i][0] = true;
+			p[i][0] = mp(i - (prev + 1), 0);
+			//used[i][0].insert(mp(prev+1,0));
+		}
+	}
+	forsn(j,1,R)
+	{
+		int prev = dp[0][j-1];
+		dp[0][j] = prev;
+		a[0][j] = false;
+		p[0][j] = p[0][j-1];
+		//unordered_set< par, pair_hash >:: iterator it;
+		//for(it = used[0][j-1].begin(); it != used[0][j-1].end(); it++)
+		//	used[0][j].insert(*it);
+		int tot = ( (prev) * (prev + 1) ) / 2;
+		if(j - tot >= (prev + 1))
+		{
+			dp[0][j]++;
+			a[0][j] = true;
+			p[0][j] = mp(0, j - (prev + 1));
+			//used[0][j].insert(mp(0,prev+1));
+		}
+	}
 
-    // Father R
-    if (r > 0 and fR.res >= fB.res) {
-        if (not isInSet(fR.rSob + 1, fR.bSob, r - 1, b)) {
-            dp[r][b] = {
-                fR.res + 1, 0, 0, fR.rSob + 1, fR.bSob, false,
-            };
-            return;
-        }
-    }
+	/*
+	forsn(i,1,B)
+	{
+		cout << i << " : " << dp[i][0] << " (" << p[i][0].f <<  "; " << p[i][0].s << ") " << a[i][0] << endl;
+	}
+	*/
 
-    // Father B
-    if (b > 0 and fB.res >= fR.res) {
-        if (not isInSet(fB.rSob, fB.bSob + 1, r, b - 1)) {
-            dp[r][b] = {
-                fB.res + 1, 0, 0, fB.rSob, fB.bSob + 1, true,
-            };
-            return;
-        }
-    }
 
-    // Default (no new juggler)
-    if (r > 0 and fR.res >= fB.res) {
-        dp[r][b] = {
-            fR.res, fR.rSob + 1, fR.bSob, 0, 0, false,
-        };
-    } else {
-        dp[r][b] = {
-            fB.res, fB.rSob, fB.bSob + 1, 0, 0, true,
-        };
-    }
-}
+	// Lleno solo B >= R
+	
+	forsn(i,1,B)
+	{
+		forsn(j,1,i+1)
+		{
+			int blue_opt = -1; int red_opt = -1;
+			int best = -1; bool add = false;
 
-void debugDP(tint r, tint b) {
-    const data d = dp[r][b];
-    cerr << r << "," << b << ": " << d.res
-        << " sob:" << d.rSob << "," << d.bSob
-        << " new:" << d.rNew << "," << d.bNew
-        << " father:" << (d.fatherB ? "B" : "R")
-        << "\n";
-}
+			forn(k, min(35,i+1))
+			{
+				forn(l, min(35,j+1))
+				{
+					if(k == 0 and l == 0) continue;
 
-void debugSet(tint r, tint b) {
-    cerr << "Set for " << r << "," << b << ":\n";
+					par cand = mp(k,l);
+					par cur = mp(i - k, j - l);
 
-    while (r > 0 or b > 0) {
-        const data d = dp[r][b];
-        if (d.rNew > 0 or d.bNew > 0)
-            cerr << "  " << d.rNew << "," << d.bNew << "\n";
+					if((dp[i-k][j-l] + 1) <= best) continue; // no hay posibilidad de mejorar y ahorramos iteraciones
 
-        if(b > 0 and d.fatherB)
-            b--;
-        else
-            r--;
-    }
-}
+					bool in = false;
 
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+					// posibilidades: para pasar de (i - k, j - l) conviene agregar (k,l) o no. Depende de si (k,l) pertenece a la cadena optima
+					// del estado (i - k, j - l). Para chequear esto, con el while recorro la cadena optima de (i - k, j - l)
 
-    tint tests = 0;
-    cin >> tests;
+					while(cur.f >= 0)
+					{
+						par next = p[cur.f][cur.s];
+						par moved = mp(cur.f - next.f, cur.s - next.s);
+						if(a[cur.f][cur.s])
+						{
+							if(moved == cand)
+							{
+								in = true;
+								break;
+							}
+						}
+						cur = next;
+					}
 
-    dp = vector<vector<data>>(maxN, vector<data>(maxN));
-    dp[0][0] = {0, 0, 0, 0, 0, false};
+					if(!in)
+					{
+						if((dp[i-k][j-l] + 1) > best)
+						{
+							best = dp[i-k][j-l] + 1;
+							blue_opt = k;
+							red_opt = l;
+							add = true;
+						}
+					}
+					else
+					{
+						if(dp[i-k][j-l] > best)
+						{
+							best = dp[i-k][j-l];
+							blue_opt = k;
+							red_opt = l;
+							add = false;
+						}
+					}
 
-    forsn(sum, 1, 2 * maxN - 1) {
-        forn(r, min(maxN, sum + 1)) {
-            if (sum - r >= maxN)
-                continue;
-            mkDP(r, sum - r);
+					/*
+					if( used[i-k][j-l].find(cand) == used[i-k][j-l].end() )
+					{
+						if((dp[i-k][j-l] + 1) > best)
+						{
+							best = dp[i-k][j-l] + 1;
+							blue_opt = k;
+							red_opt = l;
+							add = true;
+						}
+					}
+					else
+					{
+						if(dp[i-k][j-l] > best)
+						{
+							best = dp[i-k][j-l];
+							blue_opt = k;
+							red_opt = l;
+							add = false;
+						}
+					}
+					*/
+				}
+			}
 
-            const data d = dp[r][sum-r];
-        }
-    }
+			int k = blue_opt; int l = red_opt;
 
-    forsn(t, 1, tests + 1) {
-        tint r, b, res;
-        cin >> r >> b;
-        res = dp[r][b].res;
-        cout << "Case #" << t << ": " << res << "\n";
-    }
+			//unordered_set< par, pair_hash >:: iterator it;
+			//for(it = used[i-k][j-l].begin(); it != used[i-k][j-l].end(); it++)
+			//	used[i][j].insert(*it);
 
-    /*/ Print grilla
-    forn(b, 30) {
-        forn(r, 30) {
-            cerr << setw(2) << dp[r][b].res << " ";
-        }
-        cerr << "\n";
-    }/**/
+			dp[i][j] = best;
+			p[i][j] = mp(i - k, j - l);
 
-    /*/ Check symmetry
-    forn(b, maxN) {
-        forn(r, maxN) {
-            if(dp[r][b].res != dp[b][r].res) {
-                cerr << r << "," << b << "\n";
-                assert(dp[r][b].res == dp[b][r].res);
-            }
-        }
-    }
-    /**/
+			if(add)
+			{
+				a[i][j] = true;
+				//used[i][j].insert(mp(k,l));
+			}
 
-    debugDP(26,9);
-    debugSet(26,9);
-    debugDP(9,26);
-    debugSet(9,26);
+			//cout << i << " " << j << " " << dp[i][j] << endl;
+		}
+	}
 
-    return 0;
+	forsn(caso,1,t+1)
+	{
+		cin >> r >> b;
+		if(r < b) swap(r,b);
+		cout << "Case #" << caso << ": " << dp[r][b] << endl;
+	}
+
+	return 0;
 }
